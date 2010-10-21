4951d89a91f142c60a7657e8674e4ca42e802b7d
gmega <giuliano.mega@gmail.com>
2010-10-21 16:36:45 +0000

    git-svn-id: svn://localhost/tmp/repo1/trunk@130 09e1528e-719b-4268-a937-68dd4ef2d4b4

--- Shared/main/src/it/unitn/disi/utils/graph/IndexedNeighborGraph.java
+++
@@ -1,7 +0,0 @@
-package it.unitn.disi.utils.graph;
-
-import peersim.graph.Graph;
-
-public interface IndexedNeighborGraph extends Graph {
-	public int getNeighbor(int nodeIndex, int neighborIndex);
-}
