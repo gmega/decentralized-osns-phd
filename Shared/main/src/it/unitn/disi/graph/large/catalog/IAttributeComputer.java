248f2792c499fc7926cb18d07c46627527baaa79
gmega <giuliano.mega@gmail.com>
2011-02-15 12:38:30 +0000

    - Improved some docs.
    git-svn-id: svn://localhost/tmp/repo1/trunk@202 09e1528e-719b-4268-a937-68dd4ef2d4b4

--- Shared/main/src/it/unitn/disi/graph/large/catalog/IAttributeComputer.java
+++
@@ -1,7 +0,0 @@
-package it.unitn.disi.graph.large.catalog;
-
-import it.unitn.disi.graph.IndexedNeighborGraph;
-
-public interface IAttributeComputer {
-	public Number compute(IndexedNeighborGraph source, int root);
-}
