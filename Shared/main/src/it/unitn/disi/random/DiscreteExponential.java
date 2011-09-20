fc09087e878bf71675135fad7cd45cb98a8364c0
gmega <giuliano.mega@gmail.com>
2011-09-20 12:56:25 +0000

    - Made implementation less convoluted and easier to use, as well as in accordance with the P2P formulae.
    git-svn-id: svn://localhost/tmp/repo1/trunk@284 09e1528e-719b-4268-a937-68dd4ef2d4b4

--- Shared/main/src/it/unitn/disi/random/DiscreteExponential.java
+++ Shared/main/src/it/unitn/disi/random/DiscreteExponential.java
@@ -4,26 +4,34 @@ import java.util.Random;
 
 /**
  * Implements a simple discrete exponential distribution that can be
- * parameterized by a single probability value. Sampling from it can
- * be somewhat expensive.
+ * parameterized by a single probability value. We use a form of rejection
+ * sampling here, so it can be somewhat expensive.
  * 
  * @author giuliano
  */
 public class DiscreteExponential implements IDistribution {
+	
+	public static DiscreteExponential withProbability(double p, Random r) {
+		return new DiscreteExponential(p, r);
+	}
+	
+	public static DiscreteExponential withAverage(double avg, Random r) {
+		return withProbability((avg)/(avg + 1.0), r);
+	}
 
 	private final double fP;
 
 	private final Random fRandom;
-
-	public DiscreteExponential(double p, Random random) {
+	
+	private DiscreteExponential(double p, Random random) {
 		fRandom = random;
 		fP = p;
 	}
 
 	@Override
 	public double sample() {
-		int trials = 1;
-		while (fRandom.nextDouble() >= fP) {
+		int trials = 0;
+		while (fRandom.nextDouble() < fP) {
 			trials++;
 		}
 		return trials;
@@ -31,7 +39,7 @@ public class DiscreteExponential implements IDistribution {
 
 	@Override
 	public double expectation() {
-		return 1.0/(fP);
+		return 1.0 / (1.0 - fP) - 1.0;
 	}
 
 }
