5e48cf61cfefc04ffc2e5747722d2388f889f63d
gmega <giuliano.mega@gmail.com>
2011-10-05 12:01:26 +0000

    - Made ResettableFileInputStream more kosher.
    git-svn-id: svn://localhost/tmp/repo1/trunk@302 09e1528e-719b-4268-a937-68dd4ef2d4b4

--- Shared/main/src/it/unitn/disi/utils/streams/ResettableFileInputStream.java
+++ Shared/main/src/it/unitn/disi/utils/streams/ResettableFileInputStream.java
@@ -1,65 +1,84 @@
 package it.unitn.disi.utils.streams;
 
+import it.unitn.disi.utils.MiscUtils;
+
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * Decorator adding mark/reset support to {@link FileInputStream}, as well as
+ * read buffering.
+ * 
+ * @author giuliano
+ */
 public class ResettableFileInputStream extends InputStream {
 
-	private File fFile;
-	private BufferedInputStream fDelegate;
+	private FileInputStream fStream;
+
+	private BufferedInputStream fBuffer;
+
+	private long fMark = 0;
 
 	public ResettableFileInputStream(File file) throws IOException {
-		fFile = file;
-		fDelegate = new BufferedInputStream(new FileInputStream(file));
+		fStream = new FileInputStream(file);
+		fBuffer = new BufferedInputStream(fStream);
 	}
 
 	@Override
 	public int available() throws IOException {
-		return fDelegate.available();
+		return fBuffer.available();
 	}
 
 	@Override
 	public void close() throws IOException {
-		fDelegate.close();
+		fBuffer.close();
 	}
 
 	@Override
 	public synchronized void mark(int readlimit) {
-		fDelegate.mark(readlimit);
+		try {
+			long read = fStream.getChannel().position();
+			int buffered = fBuffer.available() - fStream.available();
+			fMark = read - buffered;
+		} catch (IOException ex) {
+			throw MiscUtils.nestRuntimeException(ex);
+		}
 	}
 
 	@Override
 	public boolean markSupported() {
-		return fDelegate.markSupported();
+		return true;
 	}
 
 	@Override
 	public int read() throws IOException {
-		return fDelegate.read();
+		return fBuffer.read();
 	}
 
 	@Override
 	public int read(byte[] b, int off, int len) throws IOException {
-		return fDelegate.read(b, off, len);
+		return fBuffer.read(b, off, len);
 	}
 
 	@Override
 	public int read(byte[] b) throws IOException {
-		return fDelegate.read(b);
+		return fBuffer.read(b);
 	}
 
 	@Override
 	public synchronized void reset() throws IOException {
-		fDelegate.close();
-		fDelegate = new BufferedInputStream(new FileInputStream(fFile));
+		// To reset, we need to reposition the FileInputStream
+		// and throw away the buffer.
+		fStream.getChannel().position(fMark);
+		fBuffer = new BufferedInputStream(fStream);
 	}
 
 	@Override
 	public long skip(long n) throws IOException {
-		return fDelegate.skip(n);
+		return fBuffer.skip(n);
 	}
 
 }
