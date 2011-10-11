d6989a2e7266cf7cee5af7bbf35fcc64c7585275
gmega <giuliano.mega@gmail.com>
2011-10-11 09:30:12 +0000

    - Partially fixed arg forwarding for the first plot call.
    git-svn-id: svn://localhost/tmp/repo1/trunk@312 09e1528e-719b-4268-a937-68dd4ef2d4b4

--- Shared/misc/R/graphplot.R
+++ Shared/misc/R/graphplot.R
@@ -10,10 +10,15 @@ plot_neighborhood <- function(g, root_id) {
 # Line plotting util which calls plot, then lines.
 #
 ###############################################################################
+
 setConstructorS3("LinePlotter", function(colorscale = NULL, colorcycle=20, ...) {
+	
+	pars <- match.call()
+	pars$colorscale <- pars$colorcycle <- NULL
+	
 	extend(Object(), "LinePlotter",
 			.first = TRUE,
-			.pars = ...,
+			.pars = pars,
 			.lty = 1,
 			.colorscale = colorscale(colorcycle)
 	)
@@ -25,7 +30,17 @@ setMethodS3("doPlot", "LinePlotter", function(this, formula, ...) {
 	this$.lty <- this$.lty + 1
 	
 	if(this$.first){
-		plot(formula, lty = style, type = "l", col=color, this$.pars, ...)
+		# Sets up the plot call.
+		this$.pars$lty <- style
+		this$.pars$col <- color
+		this$.pars$type <- "l"
+		this$.pars$x <- formula		
+		this$.pars[[1]] <- as.name("plot")
+		
+		# And now performs it.
+		eval(this$.pars, sys.frame(sys.parent()))
+		
+		# Done, all subsequent calls will be to lines.
 		this$.first=FALSE
 	} else {
 		lines(formula, lty = style, col=color, ...)
