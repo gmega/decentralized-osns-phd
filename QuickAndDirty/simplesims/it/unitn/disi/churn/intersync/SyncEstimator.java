ac003c64a0ffa27bfa4b02a31382bbc165eec87c
gmega <giuliano.mega@gmail.com>
2011-12-06 11:03:04 +0000

    - Made the sync sim less "quick-and-dirty".
    git-svn-id: svn://localhost/tmp/repo1/trunk@331 09e1528e-719b-4268-a937-68dd4ef2d4b4

---
+++ QuickAndDirty/simplesims/it/unitn/disi/churn/intersync/SyncEstimator.java
@@ -0,0 +1,178 @@
+package it.unitn.disi.churn.intersync;
+
+import java.util.Properties;
+
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import peersim.config.Configuration;
+import peersim.util.IncrementalStats;
+
+import it.unitn.disi.churn.RenewalProcess;
+import it.unitn.disi.churn.RenewalProcess.State;
+import it.unitn.disi.network.churn.yao.AveragesFromFile;
+import it.unitn.disi.network.churn.yao.YaoInit.IAverageGenerator;
+import it.unitn.disi.network.churn.yao.YaoInit.IDistributionGenerator;
+import it.unitn.disi.network.churn.yao.YaoPresets;
+
+public class SyncEstimator {
+
+	@Option(name = "-m", aliases = { "--mode" }, usage = "One of the Yao modes.", required = true)
+	private String fMode;
+
+	@Option(name = "-r", aliases = { "--repetitions" }, usage = "How many times to repeat the synchronization state.", required = true)
+	private int fRepeats;
+
+	@Option(name = "-c", aliases = { "--chainsize" }, usage = "Size of the chain to simulate.", required = true)
+	private int fSize;
+
+	@Option(name = "-o", aliases = { "--outer" }, usage = "Uses outer instead of inner repetitions.", required = false)
+	private boolean fUseOuter = false;
+
+	@Option(name = "-p", aliases = { "--parfile" }, usage = "File containing li/di parameters.", required = false)
+	private String fParfile;
+
+	@Option(name = "-b", aliases = { "--burnin" }, usage = "Simulation burn-in time.", required = false)
+	private double fBurnin;
+	
+	@Option(name = "-v", aliases = { "--verbose"}, usage = "Verbose session lengths.", required = false)
+	private boolean fVerbose = false;
+
+	@Option(name = "-h", aliases = { "--help" }, usage = "prints this help message", required = false)
+	private boolean fHelpOnly;
+
+	public void _main(String[] args) throws Exception {
+		CmdLineParser parser = new CmdLineParser(this);
+
+		try {
+			parser.parseArgument(args);
+			if (fHelpOnly) {
+				printHelp(parser);
+				return;
+			}
+		} catch (CmdLineException ex) {
+			System.err.println(ex.getMessage());
+			printHelp(parser);
+			return;
+		}
+
+		Configuration.setConfig(new Properties());
+
+		IAverageGenerator agen = averageGenerator();
+		IDistributionGenerator dgen = YaoPresets.mode(fMode);
+
+		System.out.println("P:p di li");
+		System.out.println("E: intersync estimate");
+
+		double[] li = new double[2], di = new double[2];
+		li[1] = agen.nextLI();
+		di[1] = agen.nextDI();
+
+		int[] pid = new int[2];
+
+		for (int i = 1; i <= fSize; i++) {
+			li[0] = li[1];
+			di[0] = di[1];
+			li[1] = agen.nextLI();
+			di[1] = agen.nextDI();
+			pid[0] = i;
+			pid[1] = i + 1;
+			IncrementalStats stats = runExperiment(agen, dgen, li, di, pid, i);
+			printResults(stats);
+		}
+
+	}
+
+	// ------------------------------------------------------------------------
+
+	private IncrementalStats runExperiment(IAverageGenerator agen,
+			IDistributionGenerator dgen, double[] li, double[] di, int[] pid,
+			int id) {
+
+		IncrementalStats stats = new IncrementalStats();
+
+		// With outer repeats, we start one experiment from scratch each time.
+		SyncExperiment exp;
+		if (fUseOuter) {
+			for (int i = 0; i < fRepeats; i++) {
+				exp = new SyncExperiment(create(pid[0], dgen, li[0], di[0],
+						i != 0), create(pid[1], dgen, li[1], di[1], i != 0),
+						fBurnin, Integer.toString(i), 1, stats, fVerbose);
+				exp.run();
+			}
+		}
+
+		else {
+			exp = new SyncExperiment(create(pid[0], dgen, li[0], di[0], false),
+					create(pid[1], dgen, li[1], di[1], false), fBurnin,
+					Integer.toString(id), fRepeats, stats, fVerbose);
+			exp.run();
+		}
+
+		return stats;
+	}
+	
+	// ------------------------------------------------------------------------
+
+	private IAverageGenerator averageGenerator() {
+		if (fParfile != null) {
+			return new AveragesFromFile(fParfile, true);
+		}
+
+		return YaoPresets.averageGenerator("yao");
+	}
+
+	// ------------------------------------------------------------------------
+
+	private void printHelp(CmdLineParser parser) {
+		System.err.println(this.getClass().getSimpleName()
+				+ " [options...] processing_class");
+		parser.printUsage(System.err);
+		System.err.println();
+	}
+
+	// ------------------------------------------------------------------------
+
+	private static RenewalProcess create(int i, IDistributionGenerator dp,
+			double li, double di, boolean quiet) {
+
+		if (!quiet) {
+			StringBuffer buf = new StringBuffer();
+			buf.append("P:");
+			buf.append(i);
+			buf.append(" ");
+			buf.append(di);
+			buf.append(" ");
+			buf.append(li);
+
+			System.out.println(buf);
+		}
+
+		return new RenewalProcess(dp.uptimeDistribution(li),
+				dp.downtimeDistribution(di), State.up);
+	}
+
+	// ------------------------------------------------------------------------
+
+	private void printResults(IncrementalStats stats) {
+		// Done. Prints the statistics for the 11 and 10 states.
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("E: ");
+
+		double lubound = stats.getAverage();
+		buffer.append(" ");
+		buffer.append(lubound);
+		buffer.append(" ");
+		buffer.append(lubound * 3600);
+		System.out.println(buffer);
+
+	}
+	
+	// ------------------------------------------------------------------------
+
+	public static void main(String[] args) throws Exception {
+		new SyncEstimator()._main(args);
+	}
+
+}
---
+++ QuickAndDirty/simplesims/it/unitn/disi/churn/intersync/SyncExperiment.java
@@ -0,0 +1,170 @@
+package it.unitn.disi.churn.intersync;
+
+import it.unitn.disi.churn.RenewalProcess;
+
+import java.util.PriorityQueue;
+
+import peersim.util.IncrementalStats;
+
+public class SyncExperiment implements Runnable {
+
+	static final byte S00 = 0x00;
+	static final byte S01 = 0x01;
+	static final byte S10 = 0x02;
+	static final byte S11 = 0x03;
+
+	/** The two RenewalProcesses we are going to synchronize. */
+	private final RenewalProcess fP1;
+	private final RenewalProcess fP2;
+
+	/** Keeps track of intersync time stats. */
+	private final IncrementalStats fLatency;
+
+	/** Current simulation time. */
+	private volatile double fTime;
+
+	/** Burn-in time. */
+	private volatile double fBurnin;
+
+	/** Start of the last desync epoch. */
+	private volatile double fLast = -1.0;
+
+	/** Current synchronization state. */
+	private volatile byte fCurrent;
+
+	private volatile int fZeroes = 0;
+
+	private volatile int fSyncs;
+
+	private final boolean fVerbose;
+
+	private final String fEid;
+
+	public SyncExperiment(RenewalProcess p1, RenewalProcess p2, double burnin,
+			String eid, int syncs, boolean verbose) {
+		this(p1, p2, burnin, eid, syncs, new IncrementalStats(), verbose);
+	}
+
+	public SyncExperiment(RenewalProcess p1, RenewalProcess p2, double burnin,
+			String eid, int syncs, IncrementalStats stats, boolean verbose) {
+		fP1 = p1;
+		fP2 = p2;
+		fBurnin = burnin;
+		fSyncs = syncs;
+		fCurrent = currentState();
+		fLatency = stats;
+		fVerbose = verbose;
+		fEid = eid;
+	}
+
+	public void run() {
+		PriorityQueue<RenewalProcess> queue = new PriorityQueue<RenewalProcess>();
+		fP1.next();
+		fP2.next();
+
+		updateSync();
+
+		queue.add(fP1);
+		queue.add(fP2);
+
+		while (!queue.isEmpty() && !done()) {
+			RenewalProcess p = queue.remove();
+			fTime = p.nextSwitch();
+			p.next();
+			queue.add(p);
+			updateSync();
+		}
+	}
+
+	private boolean isBurningIn() {
+		return fTime < fBurnin;
+	}
+
+	public IncrementalStats accounting() {
+		return fLatency;
+	}
+
+	public long zeroes() {
+		return fZeroes;
+	}
+
+	private void updateSync() {
+		byte state = currentState();
+		if (state == fCurrent) {
+			return;
+		}
+
+		// New state.
+		switch (state) {
+
+		case S10:
+			// We are not in a desync epoch.
+			// Start counting.
+			if (noDesyncEpoch()) {
+				startDesyncEpoch();
+			}
+			break;
+
+		case S11:
+			endDesyncEpoch();
+			break;
+		}
+
+		fCurrent = state;
+	}
+
+	private boolean noDesyncEpoch() {
+		return fLast < 0;
+	}
+
+	private void startDesyncEpoch() {
+		fLast = fTime;
+	}
+
+	private void endDesyncEpoch() {
+		double duration = 0.0D;
+		if (!noDesyncEpoch()) {
+			duration = fTime - fLast;
+		}
+		fLast = -1;
+		account(duration);
+	}
+
+	private void account(double duration, IncrementalStats... stats) {
+		if (isBurningIn()) {
+			if (fVerbose) {
+				System.out.println("SB: " + fEid + " " + duration);
+			}
+			return;
+		}
+
+		if (fVerbose) {
+			System.out.println("SA: " + fEid + " " + duration);
+		}
+
+		if (duration != 0.0D) {
+			fLatency.add(duration);
+		} else {
+			fZeroes++;
+		}
+		fSyncs--;
+	}
+
+	private boolean done() {
+		return fSyncs <= 0;
+	}
+
+	private byte currentState() {
+		byte state = 0;
+		if (fP1.isUp()) {
+			state |= 2;
+		}
+
+		if (fP2.isUp()) {
+			state |= 1;
+		}
+
+		return state;
+	}
+
+}
