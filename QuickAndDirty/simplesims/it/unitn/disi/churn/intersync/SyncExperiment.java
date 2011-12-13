b21e3c159d26df51fd4f4e615d7e36340cba81bd
gmega <giuliano.mega@gmail.com>
2011-12-13 21:30:08 +0000

    - Biased sync experiment.
    git-svn-id: svn://localhost/tmp/repo1/trunk@335 09e1528e-719b-4268-a937-68dd4ef2d4b4

--- QuickAndDirty/simplesims/it/unitn/disi/churn/intersync/SyncExperiment.java
+++ QuickAndDirty/simplesims/it/unitn/disi/churn/intersync/SyncExperiment.java
@@ -4,42 +4,32 @@ import it.unitn.disi.churn.RenewalProcess;
 import it.unitn.disi.churn.StateAccountant;
 
 import java.util.PriorityQueue;
+import org.junit.internal.matchers.IsCollectionContaining;
 
 import peersim.util.IncrementalStats;
 
 public class SyncExperiment implements Runnable {
 
-	static final byte SIN = 0x04;
-	static final byte S00 = 0x00;
-	static final byte S01 = 0x01;
-	static final byte S10 = 0x02;
-	static final byte S11 = 0x03;
-
 	/** The two RenewalProcesses we are going to synchronize. */
 	private final RenewalProcess fP1;
 	private final RenewalProcess fP2;
-	
-	private final StateAccountant f10A = new StateAccountant();
-	private final StateAccountant f11A = new StateAccountant();
-
-	/** Keeps track of intersync time stats. */
-	private final IncrementalStats fLatency;
 
 	/** Current simulation time. */
 	private volatile double fTime;
 
-	/** Burn-in time. */
-	private volatile double fBurnin;
+	private StateAccountant fWaitSync;
 
-	/** Start of the last desync epoch. */
-	private volatile double fLast = -1.0;
+	private boolean fWaitingLogin = true;
 
-	/** Current synchronization state. */
-	private volatile byte fCurrent;
-
-	private volatile int fZeroes = 0;
+	private boolean fWaitingSync = false;
 
 	private volatile int fSyncs;
+
+	private boolean fDone;
+	
+	private double fIdle = -1;
+	
+	private double fIdleMax;
 
 	private final boolean fVerbose;
 
@@ -54,166 +44,67 @@ public class SyncExperiment implements Runnable {
 			String eid, int syncs, IncrementalStats stats, boolean verbose) {
 		fP1 = p1;
 		fP2 = p2;
-		fBurnin = burnin;
 		fSyncs = syncs;
-		fCurrent = currentState();
-		fLatency = stats;
 		fVerbose = verbose;
 		fEid = eid;
+		fIdleMax = burnin;
+		fWaitSync = new StateAccountant(stats, new IncrementalStats());
 	}
 
 	public void run() {
 		PriorityQueue<RenewalProcess> queue = new PriorityQueue<RenewalProcess>();
 		fP1.next();
 		fP2.next();
-
-		fCurrent = currentState();
-		updateSync(SIN, fCurrent);
-		updateAccounting(SIN, fCurrent);
-
 		queue.add(fP1);
 		queue.add(fP2);
 
-		while (!queue.isEmpty() && !done()) {
+		while (!queue.isEmpty() && !fDone) {
 			RenewalProcess p = queue.remove();
 			fTime = p.nextSwitch();
 			p.next();
 			queue.add(p);
-			
-			byte newState = currentState();
-			updateSync(fCurrent, newState);
-			updateAccounting(fCurrent, newState);
-			fCurrent = newState;
+			stateShifted(p);
+		}
+	}
+
+	private void stateShifted(RenewalProcess p) {
+		
+		if (fTime < fIdle) {
+			return;
+		}
+		
+		// We saw a login event for P1.
+		if (fWaitingLogin) {
+			if (p == fP1 && p.isUp()) {
+				fWaitingLogin = false;
+				fWaitingSync = true;
+				fWaitSync.enterState(fTime);
+			}
+		}
+
+		// P1 and P2 are synchronized.
+		if (fWaitingSync) {
+			if (fP1.isUp() && fP2.isUp()) {
+				fWaitSync.exitState(fTime);
+				fSyncs--;
+				fWaitingLogin = true;
+				fWaitingSync = false;
+				// Begin idle period.
+				fIdle = fTime + idlePeriod();
+			}
+		}
+
+		if (fSyncs == 0) {
+			fDone = true;
 		}
 	}
 	
+	private double idlePeriod() {
+		return fIdleMax;
+	}
+
 	public double time() {
 		return fTime;
-	}
-
-	public IncrementalStats accounting() {
-		return fLatency;
-	}
-	
-	public StateAccountant s10Accountant() {
-		return f10A;
-	}
-	
-	public StateAccountant s11Accountant() {
-		return f11A;
-	}
-
-	public long zeroes() {
-		return fZeroes;
-	}
-	
-	private void updateAccounting(byte oldState, byte newState) {
-		if (newState == oldState) {
-			return;
-		}
-		
-		switch(oldState) {
-		
-		case S10:
-			f10A.exitState(fTime);
-			break;
-		
-		case S11:
-			f11A.exitState(fTime);
-			break;
-			
-		}
-		
-		switch(newState) {
-		
-		case S10:
-			f10A.enterState(fTime);
-			break;
-			
-		case S11:
-			f11A.enterState(fTime);
-			break;
-			
-		}
-	}
-
-	private void updateSync(byte oldState, byte newState) {
-		if (newState == oldState) {
-			return;
-		}
-
-		// New state.
-		switch (newState) {
-
-		case S10:
-			// We are not in a desync epoch.
-			// Start counting.
-			if (noDesyncEpoch()) {
-				startDesyncEpoch();
-			}
-			break;
-
-		case S11:
-			endDesyncEpoch();
-			break;
-		
-		}
-	}
-
-	private boolean noDesyncEpoch() {
-		return fLast < 0;
-	}
-
-	private void startDesyncEpoch() {
-		fLast = fTime;
-	}
-
-	private void endDesyncEpoch() {
-		double duration = 0.0D;
-		if (!noDesyncEpoch()) {
-			duration = fTime - fLast;
-		}
-		account(duration);
-		fLast = -1;
-	}
-
-	private void account(double duration, IncrementalStats... stats) {
-		// If the start time for the current epoch is behind the
-		// burn-in mark, don't account.
-//		if (fLast < fBurnin) {
-//			if (fVerbose) {
-//				System.out.println("SB: " + fEid + " " + duration);
-//			}
-//			return;
-//		}
-
-		if (fVerbose) {
-			System.out.println("SA: " + fEid + " " + duration);
-		}
-
-		if (duration != 0.0D) {
-			fLatency.add(duration);
-		} else {
-			fZeroes++;
-		}
-		fSyncs--;
-	}
-
-	private boolean done() {
-		return fSyncs <= 0;
-	}
-
-	private byte currentState() {
-		byte state = 0;
-		if (fP1.isUp()) {
-			state |= 2;
-		}
-
-		if (fP2.isUp()) {
-			state |= 1;
-		}
-
-		return state;
 	}
 
 }
